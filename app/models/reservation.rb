b540cd6a8d7b425fd39f90e8967381f8637cb348
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-10-03 12:35:19 +0200

    add reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  validates :user, presence: true
+  validates :course, presence: true
+
+  validate :valid_date_range
 end
