c1ad6c516f1ef20abc0f4835aca5329962166dc1
gisa chris <gisachris24@gmail.com>
2023-10-11 15:29:53 -0200

    Update seed file with appropriate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,24 +4,23 @@
 user1 = User.create(name: "John Doe")
 user2 = User.create(name: "Jane Smith")
 
-# Create an array of image URLs
-# image_urls = ['/course/database.png', '/course/web.png']
+Course.create(
+  name: 'React18 Full Course',
+  description: 'This is a React18 complete full course to take you from a noob to a pro',
+  fee: 1200,
+  startDate: "2024-04-04"
+).image.attach(io: File.open('assets/images/react.png'), filename: 'react.png', content_type: 'image/png')
 
-# Create sample courses
-# courses = []
-# 6.times do |i|
-#   courses << Course.create(
-#     name: "Course #{i + 1}",
-#     description: "Description for Course #{i + 1}",
-#     image: image_urls[i % 2], # Alternating between the two images
-#     fee: 100.0 + (i * 10),     # Incrementing fee
-#     startDate: Date.new(2023, 10, 10) + i.days  # Incrementing start date
-#   )
-# end
-Course.create(name: 'course_1', description:'this is our first course', fee: 1200, startDate: "2023-04-04")
+Course.create(
+  name: 'HTML Beginner Course',
+  description: 'This is A beginner Course in HTML and all you need to know to get started with the Web Structuring Language',
+  fee: 1500,
+  startDate: "2023-04-06"
+).image.attach(io: File.open('assets/images/html.png'), filename: 'html.png', content_type: 'image/png')
 
-course = Course.first
-
-# Create sample reservations associated with users and courses
-Reservation.create(user_id: user1.id, course_id: course.id, city: "New York", date: Date.new(2023, 10, 12))
-Reservation.create(user_id: user2.id, course_id: course.id, city: "Los Angeles", date: Date.new(2023, 11, 8))
+Course.create(
+  name: 'CSS Beginner Course',
+  description: 'This is A cascading style sheet introduction course to HTML web page design and its different use cases',
+  fee: 1500,
+  startDate: "2023-04-06"
+).image.attach(io: File.open('assets/images/css.png'), filename: 'css.png', content_type: 'image/png')
