d2c579d74cd18b12542b62b8cba3360d0cf1c7a2
gisa chris <gisachris24@gmail.com>
2023-10-12 18:47:01 -0200

    update Cors file with site link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
+    origins "https://edu-planner.onrender.com"
 
     resource "*",
       headers: :any,
