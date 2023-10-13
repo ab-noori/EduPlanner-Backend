eabf1db7e80fcc90a0cf27de073e9998dc3851c8
gisa chris <gisachris24@gmail.com>
2023-10-13 00:14:53 -0200

    change cors settings for user Signup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://edu-planner.onrender.com"
+    origins "*"
 
     resource "*",
       headers: :any,
